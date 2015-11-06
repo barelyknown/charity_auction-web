3755e7d875e047728cef85922f5781a3236f209d
Sean Devine <barelyknown@icloud.com>
2015-11-06 12:02:40 -0500

    add minimum bid amount to auction item

--- app/models/auction-item.js
+++ app/models/auction-item.js
@@ -6,6 +6,7 @@ export default DS.Model.extend({
   donation: DS.belongsTo('donation'),
   bidType: DS.belongsTo('bid-type'),
   bidGroup: DS.belongsTo('bid-group'),
+  minimumBidAmount: DS.attr('number'),
 
   isAuctionValid: Ember.computed.notEmpty('auction.content'),
   isDonationValid: Ember.computed.notEmpty('donation.content'),
